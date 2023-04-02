fa6d0e31949742bbcb833a2b4611f1e58742b89c
2021ct93045 <2021ct93045@wilp.bits-pilani.ac.in>
2023-04-02 08:33:10 +0530

    server address

--- src/main/java/com/group9/controller/HealthCheckController.java
+++ src/main/java/com/group9/controller/HealthCheckController.java
@@ -1,9 +1,10 @@
 package com.group9.controller;
 
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheckController {
 
 	@Autowired
-    private Environment environment;
+	private ServletWebServerApplicationContext server;
 	
     @GetMapping("/health")
     public String checkHealth(){
-    	
-    	String currentIp = InetAddress.getLoopbackAddress().getHostAddress();
-        return  "weatherApp is UP and Running on "+currentIp;
+
+    	String currentAddress="localhost";
+		try {
+			currentAddress = InetAddress.getLocalHost().getHostAddress();
+			
+		} catch (UnknownHostException e) {
+			
+		}		
+        return  "weatherApp is UP and Running on "+currentAddress;
+
     }
 }
